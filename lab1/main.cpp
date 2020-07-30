014c116bca3f9852ccccc8c1443467707ccdac67
Kabanova Ekaterina <katovskaya009@gmail.com>
2020-07-30 14:02:57 +0700

    test(Breakpoints): добавлен комментарий со значениями переменной sum на каждой итерации цикла

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -9,6 +9,7 @@ void Breakpoints()
     double sum = 0.0;
     for (int i = 0; i < 10; i++)
     {
+        // sum = 0.0, 1.1, 3.52, 7.513, 13.3, 21.42, 31.05, 45.69, 62.84, 84.06
         sum += add * i;
         add *= 1.1;
     }
@@ -19,7 +20,7 @@ void main()
 {
 	setlocale(LC_ALL, "rus");
 
-    // Задание 1.1.1..
+    // Задание 1.1.1.1
     Breakpoints();
 }
 
