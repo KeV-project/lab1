d3bbee3a9fc48c7067ccea2ee9b061fd8a8a0e82
Kabanova Ekaterina <katovskaya009@gmail.com>
2020-08-05 22:48:12 +0700

    feature(lab2): добавлена структура Flight

---
+++ lab2/lab2/lab2/Flight.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <iostream>
+
+using namespace std;
+
+struct Flight
+{
+	int PlaneNumber;
+	// Пункт Вылета
+	string Departure;
+	// Пункт назначения
+	string Destination;
+	// Время полета в минутах
+	int Duration;
+	int Distance;
+	//Количество пассажиров на борту
+	int Passengers;
+};
