e5ebc9e3d7e6b451acf1c56f788956cbaea2dc32
Kabanova Ekaterina <katovskaya009@gmail.com>
2020-08-17 21:13:47 +0700

    feature(lab3): добавлен класс Song

---
+++ lab3/lab3/Song.cpp
@@ -0,0 +1,52 @@
+#include "Song.h"
+
+void Song::SetTitle(const string& title)
+{
+	this->_title = title;
+}
+
+void Song::SetDuration(const int duration)
+{
+	if (duration < 0)
+	{
+		string message = "Отрицательное число " + to_string(duration)
+			+ " не может определять продолжительность песни";
+		char buf[255];
+		strcpy_s(buf, message.c_str());
+		throw exception(buf);
+	}
+	this->_duration = duration;
+}
+
+void Song::SetGenre(const GenreType& genre)
+{
+	this->_genre = genre;
+}
+
+string Song::GetTitle()
+{
+	return this->_title;
+}
+float Song::GetDuration()
+{
+	return this->_duration;
+}
+
+GenreType Song::GetGenre()
+{
+	return this->_genre;
+}
+
+Song::Song()
+{
+	SetTitle("");
+	SetDuration(0);
+	SetGenre(Jazz);
+}
+
+Song::Song(const string& title, const int duration, const GenreType& genre)
+{
+	SetTitle(title);
+	SetDuration(duration);
+	SetGenre(genre);
+}
---
+++ lab3/lab3/Song.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <string>
+#include "GenreType.h"
+
+using namespace std;
+
+class Song
+{
+private:
+	string _title;
+	int _duration;
+	GenreType _genre;
+
+public:
+	void SetTitle(const string& title);
+	void SetDuration(const int duration);
+	void SetGenre(const GenreType& genre);
+
+	string GetTitle();
+	float GetDuration();
+	GenreType GetGenre();
+
+	Song();
+	Song(const string& title, const int duration, const GenreType& genre);
+};
