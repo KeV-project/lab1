51d297e17112831bc7ef74dbaa4b6c00bbf521d3
Kabanova Ekaterina <katovskaya009@gmail.com>
2020-08-20 20:03:20 +0700

    feature(lab5): создан класс Person

---
+++ lab5/lab5/Person.cpp
@@ -0,0 +1,47 @@
+#include "Person.h"
+
+void Person::SetName(const string& name)
+{
+	this->_name = name;
+}
+
+void Person::SetSurname(const string& surname)
+{
+	this->_surname = surname;
+}
+
+void Person::SetPatronymic(const string& patronymic)
+{
+	this->_patronymic = patronymic;
+}
+
+string Person::GetName()
+{
+	return _name;
+}
+
+string Person::GetSurname()
+{
+	return _surname;
+}
+
+string Person::GetPatronymic()
+{
+	return _patronymic;
+}
+
+Person::Person()
+{
+	SetName("");
+	SetSurname("");
+	SetPatronymic("");
+}
+
+Person::Person(const string& name, const string& surname, 
+	const string& patronymic)
+{
+	SetName(name);
+	SetSurname(surname);
+	SetPatronymic(patronymic);
+}
+
---
+++ lab5/lab5/Person.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <string>
+using namespace std;
+class Person
+{
+private:
+	string _name;
+	string _surname;
+	string _patronymic;
+
+public:
+	void SetName(const string& name);
+	void SetSurname(const string& surname);
+	void SetPatronymic(const string& patronymic);
+
+	string GetName();
+	string GetSurname();
+	string GetPatronymic();
+
+	Person();
+	Person(const string& name, const string& surname, 
+		const string& patronymic);
+};
+
