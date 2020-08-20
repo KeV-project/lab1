09788a88b4aad58f42c11b447f8ce1693a439dee
Kabanova Ekaterina <katovskaya009@gmail.com>
2020-08-20 18:52:40 +0700

    feature(lab4): доблен класс CollisionManager

--- lab4/lab4/CollisionManager.cpp
+++ lab4/lab4/CollisionManager.cpp
@@ -1,1 +1,25 @@
 #include "CollisionManager.h"
+
+bool CollisionManager::IsCollision(Rect& rect1, Rect& rect2)
+{
+	if (abs(rect1.GetCentreX() - rect2.GetCentreX()) <
+		((abs(rect1.GetLength() + rect2.GetLength())) / 2) &&
+		abs(rect1.GetCentreY() - rect2.GetCentreY()) <
+		(abs(rect1.GetWidth() + rect2.GetWidth()) / 2))
+	{
+		return true;
+	}
+	return false;
+}
+
+bool CollisionManager::IsCollision(Ring& ring1, Ring& ring2)
+{
+	int c = sqrt(pow(ring1.GetCentreX() - ring2.GetCentreX(), 2) +
+		pow(ring1.GetCentreY() - ring2.GetCentreY(), 2));
+	if (c < (ring1.GetOutRadius() + ring2.GetOutRadius()))
+	{
+		return true;
+	}
+	return false;
+}
+
--- lab4/lab4/CollisionManager.h
+++ lab4/lab4/CollisionManager.h
@@ -1,5 +1,13 @@
 #pragma once
-class CollisionManager
+#include <math.h> 
+#include "Rect.h"
+#include "Ring.h"
+
+static class CollisionManager
 {
+public:
+	static bool IsCollision(Rect& rect1, Rect& rect2);
+	static bool IsCollision(Ring& ring1, Ring& ring2);
 };
+
 
