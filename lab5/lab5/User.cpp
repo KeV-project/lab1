e9215101a3090afc10b12989314f30317d8f4288
Kabanova Ekaterina <katovskaya009@gmail.com>
2020-08-29 19:25:07 +0700

    feature(lab5): добавлен класс User

--- lab5/lab5/User.cpp
+++ lab5/lab5/User.cpp
@@ -1,1 +1,58 @@
 #include "User.h"
+
+void User::SetId(const int id)
+{
+	_id = id;
+}
+void User::SetLogin(const string& login)
+{
+	if (login.find("}") != -1 || login.find("{") != -1 
+		|| login.find(">") != -1 || login.find("<") != -1 
+		|| login.find("@") != -1 || login.find("#") != -1 
+		|| login.find("$") != -1 || login.find("%") != -1 
+		|| login.find("^") != -1 || login.find(":") != -1 
+		|| login.find("*") != -1)
+	{
+		throw exception("Логин не должен содержать следующие символы:"
+		"{, }, <, >, @, # , $, %, ^, :, *");
+	}
+	_login = login;
+}
+void User::SetPassword(const string& password)
+{
+	_password = password;
+}
+
+int User::GetId() 
+{ 
+	return _id; 
+}
+
+string User::GetLogin()
+{ 
+	return _login; 
+}
+
+string User::GetPassword() 
+{
+	return _password; 
+}
+
+User::User()
+{
+	SetLogin("");
+	SetPassword("");
+	SetId(0);
+}
+
+User::User(const int id, const string& login, const string& password)
+{
+	SetId(id);
+	SetLogin(login);
+	SetPassword(password);
+}
+bool User::IsCorrectPassword(const string& password)
+{
+	return (password == _password);
+}
+
--- lab5/lab5/User.h
+++ lab5/lab5/User.h
@@ -1,5 +1,28 @@
 #pragma once
+#include <string>
+using namespace std;
+
 class User
 {
+private:
+	string _login;
+	string _password;
+
+protected:
+	int _id;
+	void SetId(const int id);
+
+public:
+	void SetLogin(const string& login);
+	void SetPassword(const string& password);
+
+	int GetId();
+	string GetLogin();
+	string GetPassword();
+
+	User();
+	User(const int id, const string& login, const string& password);
+
+	bool IsCorrectPassword(const string& password);
 };
 
