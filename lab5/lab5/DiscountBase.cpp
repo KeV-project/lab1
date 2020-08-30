13a2d8b78688e060f630389ed1e744e72be30b66
Kabanova Ekaterina <katovskaya009@gmail.com>
2020-08-30 12:59:52 +0700

    feature(lab5): добавлен абстрактный класс DiscountBase

---
+++ lab5/lab5/DiscountBase.cpp
@@ -0,0 +1,17 @@
+#include "DiscountBase.h"
+
+void DiscountBase::SetCategory(const CategoryType& category)
+{
+	this->_category = category;
+}
+
+CategoryType DiscountBase::GetCategory()
+{
+	return _category;
+}
+
+DiscountBase::DiscountBase(const CategoryType& category)
+{
+	SetCategory(category);
+}
+
---
+++ lab5/lab5/DiscountBase.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "CategoryType.h"
+#include "Product.h"
+
+class DiscountBase
+{
+private:
+	CategoryType _category;
+	void SetCategory(const CategoryType& category);
+
+public:
+	CategoryType GetCategory();
+	virtual double Calculate(Product* product) = 0;
+
+protected:
+	DiscountBase(const CategoryType& category);
+};
+
+
+
