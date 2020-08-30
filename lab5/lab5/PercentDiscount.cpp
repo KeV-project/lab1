dac4d0cd4292942cec15c1c5caafb6941519c037
Kabanova Ekaterina <katovskaya009@gmail.com>
2020-08-30 13:10:11 +0700

    feature(lab5): добавлен класс PercentDiscount

---
+++ lab5/lab5/PercentDiscount.cpp
@@ -0,0 +1,35 @@
+#include "PercentDiscount.h"
+
+void PercentDiscount::SetPercent(const int percent)
+{
+	if (percent < 0 || percent > 100)
+	{
+		string message = "Число " + to_string(percent)
+			+ " не входит в допустимый диапазон [0, 100]"
+			+ "\nи не может определять размер скидки в процентах";
+		char buf[255];
+		strcpy_s(buf, message.c_str());
+		throw exception(buf);
+	}
+	this->_percent = percent;
+}
+
+int PercentDiscount::GetPercent()
+{
+	return _percent;
+}
+
+PercentDiscount::PercentDiscount(const int percent,
+	const CategoryType& category) : DiscountBase(category)
+{
+	SetPercent(percent);
+}
+
+double PercentDiscount::Calculate(Product* product)
+{
+	if (product->GetCategory() == this->GetCategory())
+	{
+		return product->GetCost() - product->GetCost() * this->GetPercent() / 100;
+	}
+	return product->GetCost();
+}
---
+++ lab5/lab5/PercentDiscount.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "DiscountBase.h"
+
+class PercentDiscount : public DiscountBase
+{
+private:
+	int _percent;
+
+public:
+	void SetPercent(const int percent);
+
+	int GetPercent();
+
+	PercentDiscount(const int percent, const CategoryType& category);
+
+	double Calculate(Product* product) override;
+};
+
+
