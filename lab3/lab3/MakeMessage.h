94a8f9590aeb05299902be83752ac7603ff8610c
Kabanova Ekaterina <katovskaya009@gmail.com>
2020-09-04 17:41:47 +0700

    feature(lab3): добавлено перечисление MessageType

---
+++ lab3/lab3/MakeMessage.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+string MakeMassage(const int minLimit, const int maxLimit);
+string MakeMassage(const bool a);
---
+++ lab3/lab3/MessageType.h
@@ -0,0 +1,8 @@
+#pragma once
+
+enum MessageType
+{
+	NotInRange,
+	NotPositive,
+	NotNonNegative
+};
